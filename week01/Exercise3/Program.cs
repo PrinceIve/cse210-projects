19b5fa25e43feec9ec5d1c40b2c37983a27e55d9
princeive <ieriabie@byupathway.edu>
2025-03-08 21:15:47 +0100

    Guess the number

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        int guess = 0;
+        while (guess != number)
+        {
+        Console.Write("What is your guess? ");
+        string gues = Console.ReadLine();
+        guess = int.Parse(gues);
+
+        if (guess > number)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (guess < number)
+        {
+            Console.WriteLine("Higher");
+        }
+        else
+        {
+            Console.WriteLine("You guessed right!!!");
+        }
+        }
     }
 }
