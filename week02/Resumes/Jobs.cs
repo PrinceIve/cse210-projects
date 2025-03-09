af153b8c087886137b5809366611b0e1142beb20
princeive <ieriabie@byupathway.edu>
2025-03-09 23:36:35 +0100

    Abstration Assignment

---
+++ week02/Resumes/Jobs.cs
@@ -0,0 +1,13 @@
+public class Job
+{
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    public void DisplayJobDetails()
+    {
+        Console.WriteLine($"{_jobTitle} at ({_company}) {_startYear}-{_endYear}");
+    }
+
+}
